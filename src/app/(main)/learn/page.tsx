96321ffa983ad81e2ea04811b4fb518fb7e3da4f
Geraldo Luiz <geraldobl58@gmail.com>
2024-03-19 14:13:57 -0300

    feat: Add drizzle orm and configuration db

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,7 +1,23 @@
+import { FeedWarpper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
+import { Header } from "./_components/header";
+
 const LearnPage = () => {
   return (
-    <div>
-      <div>LearnPage</div>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWarpper>
+        <Header title="Spanish" />
+      </FeedWarpper>
     </div>
   );
 };
