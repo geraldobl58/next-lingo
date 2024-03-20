13543981a21e20735392f5d178357e7adfe5fb57
Geraldo Luiz <geraldobl58@gmail.com>
2024-03-20 18:17:52 -0300

    feat: Add unit component

---
+++ src/app/(main)/learn/_components/unit.tsx
@@ -0,0 +1,54 @@
+import { UnitBanner } from "./unit-banner";
+import { LessonButton } from "./lesson-button";
+
+import { lessons, units } from "@/db/schema";
+
+interface UnitProps {
+  id: number;
+  order: number;
+  description: string;
+  title: string;
+  lessons: (typeof lessons.$inferSelect & {
+    completed: boolean;
+  })[];
+  activeLesson:
+    | (typeof lessons.$inferSelect & {
+        unit: typeof units.$inferSelect;
+      })
+    | undefined;
+  activeLessonPercentage: number;
+}
+
+export const Unit = ({
+  id,
+  order,
+  description,
+  title,
+  lessons,
+  activeLesson,
+  activeLessonPercentage,
+}: UnitProps) => {
+  return (
+    <>
+      <UnitBanner title={title} description={description} />
+      <div className="flex flex-col items-center relative">
+        {lessons.map((lesson, index) => {
+          const isCurrent = lesson.id === activeLesson?.id;
+          const isLocked = !lesson.completed && !isCurrent;
+
+          return (
+            <LessonButton
+              key={lesson.id}
+              id={lesson.id}
+              index={index}
+              totalCount={lessons.length - 1}
+              current={isCurrent}
+              locked={isLocked}
+              percentage={activeLessonPercentage}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
